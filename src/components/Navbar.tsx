584c0f8ba25172ebf1bf18ecae88bfe6c0cb61f6
maritocuate <mario300@gmail.com>
2024-01-25 20:38:18 -0300

    core: kinde login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { buttonVariants } from './ui/button'
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
 
 const Navbar = () => {
   return (
@@ -22,6 +24,21 @@ const Navbar = () => {
               >
                 Pricing
               </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: 'sm',
+                })}
+              >
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
             </>
           </div>
         </div>
