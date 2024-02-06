c29c44681235282bcb1a64bc96a27c67dd0e1a98
maritocuate <mario300@gmail.com>
2024-02-06 17:46:39 -0300

    chore: dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,14 @@
+import Dashboard from './components/Dashboard'
+import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
-import Dashboard from './components/Dashboard'
 
 const Page = async () => {
   const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const user = await getUser()
 
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  console.log(user.id)
 
   const dbUser = await db.user.findFirst({
     where: {
