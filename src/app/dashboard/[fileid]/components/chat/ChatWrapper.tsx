4daf326c4d9fba81e7fbfc575590b774aa81c7e5
maritocuate <mario300@gmail.com>
2024-02-07 11:46:44 -0300

    chore: ChatWrapper component

---
+++ src/app/dashboard/[fileid]/components/chat/ChatWrapper.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Messages from './Messages'
+import ChatInput from './ChatInput'
+
+interface ChatWrapperProps {
+  fileId: string
+  isSubscribed: boolean
+}
+
+const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
+  return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 justify-between flex flex-col mb-28">
+        <Messages />
+      </div>
+
+      <ChatInput />
+    </div>
+  )
+}
+
+export default ChatWrapper
--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,10 +1,22 @@
+import ChatWrapper from './components/chat/ChatWrapper'
 import PdfRenderer from './components/PdfRenderer'
 
 export default function Page() {
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-        <PdfRenderer url="https://utfs.io/f/c42e7e83-38fe-43ea-abf7-cbf6868ef477-uhu4vd.pdf" />
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer url="https://utfs.io/f/c42e7e83-38fe-43ea-abf7-cbf6868ef477-uhu4vd.pdf" />
+          </div>
+        </div>
+
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper
+            fileId="c42e7e83-38fe-43ea-abf7-cbf6868ef477"
+            isSubscribed
+          />
+        </div>
       </div>
     </div>
   )
