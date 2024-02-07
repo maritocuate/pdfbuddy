9dce1c235a51cd7fe63ea9550f3b8953799da2aa
maritocuate <mario300@gmail.com>
2024-02-06 23:22:04 -0300

    chore: file page

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,10 @@
+import PdfRenderer from './components/PdfRenderer'
+
+export default function Page() {
+  return (
+    <>
+      File{' '}
+      <PdfRenderer url="https://utfs.io/f/c42e7e83-38fe-43ea-abf7-cbf6868ef477-uhu4vd.pdf" />
+    </>
+  )
+}
